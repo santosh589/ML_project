84aefef1a78e5a9e386fbc93adef58dd98deb6d3
santosh589 <santoshhr159@gmail.com>
2023-08-05 00:48:41 +0530

    EDA and Problems statement

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n", " ") for req in requirements]
@@ -27,8 +27,5 @@ author='Santosh',
 author_email='santoshhr159@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
-
-
 
 )
