976ceb5a62283b7c03dfaa3bc36e64279e00f3e8
santosh589 <santoshhr159@gmail.com>
2023-10-06 12:38:47 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -16,27 +16,35 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models, param):
     try:
 
         report={}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) #Train model
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train, y_train) #Train model
 
             y_train_pred= model.predict(X_train)
-            print(y_train)
+            #print(y_train)
             y_test_pred= model.predict(X_test)
-            print(y_test_pred)
+            #print(y_test_pred)
 
             train_model_score=r2_score(y_train, y_train_pred)
             test_model_score=r2_score(y_test,y_test_pred)
@@ -52,7 +60,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 def load_object(file_path):
     try:
         with open(file_path, 'rb') as file_obj:
-            return dill.load(file_obj)
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
     
